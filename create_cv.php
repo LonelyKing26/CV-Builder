02f761d70aaa783a9b6aca18e486cdc4519fe3eb
Arun Pandiyan <41441589+LonelyKing26@users.noreply.github.com>
2022-12-04 18:52:50 +0530

    Add files via upload

---
+++ create_cv.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Create CV</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	<header>
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Resume Master</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+				<a class="nav-link" href="#">Dashboard</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#">Create CV</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#">List CV</a>
+				</li>    
+			</ul>
+			</div>
+		</div>
+	</nav>
+	</header>
+	<div class="container mt-5">
+		<div class="page_title text-center">
+			<h2>Create Resume</h2>
+		</div>
+		<section class="cv_form mt-4">
+			<div class="heading">
+				<b>Personal Details</b>
+			</div>
+			<div class="personal_details">
+				<div class="row">
+					<div class="col-lg-5 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#name">Name</label>
+							<input type="text" name="" id="name" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#dob">DOB</label>
+							<input type="date" name="" id="dob" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-2 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#mobile">Mobile</label>
+							<input type="text" name="" id="mobile" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-2 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#email">Email</label>
+							<input type="text" name="" id="email" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#father_name">Father Name</label>
+							<input type="text" name="" id="father_name" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#mother_name">Mother Name</label>
+							<input type="text" name="" id="mother_name" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-6 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#address">Address</label>
+							<input type="text" name="" id="address" class="form-control">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="cv_form mt-4">
+			<div class="heading">
+				<b>Education</b>
+			</div>
+			<div class="education">
+				<div class="row">
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#degree">Degree</label>
+							<input type="text" name="" id="degree" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#course">Course</label>
+							<input type="text" name="" id="course" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#pass_year">Passed out Year</label>
+							<input type="text" name="" id="pass_year" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#percentage">Percentage</label>
+							<input type="text" name="" id="percentage" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-6 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#clg_name">School/Collage Name</label>
+							<input type="text" name="" id="clg_name" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#clg_district">District</label>
+							<input type="text" name="" id="clg_district" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#clg_country">Country</label>
+							<input type="text" name="" id="clg_country" class="form-control">
+						</div>
+					</div>	
+				</div>
+					
+			</div>
+		</section>
+		<section class="cv_form mt-4">
+			<div class="heading">
+				<b>Expereance</b>
+			</div>
+			<div class="expereance">
+				<div class="row">
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#degree">Degree</label>
+							<input type="text" name="" id="degree" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#course">Course</label>
+							<input type="text" name="" id="course" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#pass_year">Passed out Year</label>
+							<input type="text" name="" id="pass_year" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#percentage">Percentage</label>
+							<input type="text" name="" id="percentage" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-6 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#clg_name">School/Collage Name</label>
+							<input type="text" name="" id="clg_name" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#clg_district">District</label>
+							<input type="text" name="" id="clg_district" class="form-control">
+						</div>
+					</div>
+					<div class="col-lg-3 col-xs-12 mt-3">
+						<div class="form-group">
+							<label for="#clg_country">Country</label>
+							<input type="text" name="" id="clg_country" class="form-control">
+						</div>
+					</div>	
+				</div>
+					
+			</div>
+		</section>
+	</div>
+</body>
+</html>
